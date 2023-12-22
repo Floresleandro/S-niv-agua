934a739ba1f007dec96ba3de8a48b0a3a99bc82b
Floresleandro <81982968+Floresleandro@users.noreply.github.com>
2023-12-22 14:35:46 -0300

    Create lcd.h

---
+++ Librerias/en C/lcd.h
@@ -0,0 +1,23 @@
+{
+    "name": "RPi Pico LCD",
+    "version": "1.0.0",
+    "description": "Biblioteca para usar un display un adaptador I2C en la Raspberry Pi Pico con el SDK.",
+    "keywords": "rp2040, Raspberry Pi Pico, baremetal, I2C",
+    "repository":
+    {
+      "type": "git",
+      "url": "https://github.com/carlassaraf/rpipico-lcd.git"
+    },
+    "authors":
+    [
+      {
+        "name": "Fabrizio Carlassara",
+        "email": "fabrizio.carlassara@gmail.com",
+        "url": ""
+      }
+    ],
+    "license": "MIT",
+    "homepage": "",
+    "frameworks": ["baremetal"],
+    "platforms": ["WizIO-pico"]
+  }
